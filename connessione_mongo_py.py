950975cbfccc94059b9b63b717611eb234a45f46
alexandraazzena <168538339+alexandraazzena@users.noreply.github.com>
2024-07-09 14:03:23 +0200

    prima bozza ricerca

--- connessione_mongo_py.py
+++ connessione_mongo_py.py
@@ -12,3 +12,48 @@ try:
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
+
+db = client['ticket_one']
+collection = db['concerti']
+
+def ricerca_per_artista(artista):
+    concerti = collection.find({'artista.nome': artista})
+    return list(concerti)
+
+
+def ricerca_per_concerto(concerto):
+    concerti = collection.find({'concerto.nome': concerto})
+    return list(concerti)
+
+
+def main():
+    while True:
+        print("\nMenu:")
+        print("1| Cerca concerto per artista")
+        print("2| Cerca concerto per nome")
+        print("3| Esci")
+
+        scelta = input("Seleziona un'opzione: ")
+
+        if scelta == '1':
+            input_artista = input("Inserisci il nome dell'artista:  ")
+            concerti_con_artista = ricerca_per_artista(input_artista)
+            for concerto in concerti_con_artista:
+                print(concerti_con_artista)
+
+        elif scelta == '2':
+            input_concerto = input("Inserisci il nome del concerto: ")
+            concerti_con_nome = ricerca_per_concerto(input_concerto)
+            for concerto in concerti_con_nome:
+                print(concerto)
+
+
+        elif scelta == '3':
+            break
+
+        else:
+            print("Opzione non valida. Riprova.")
+
+
+if __name__ == "__main__":
+    main()
