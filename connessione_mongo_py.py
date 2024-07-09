bc3c37b23a3a07db425fca8fb2cfba14730f14f5
alexandraazzena <168538339+alexandraazzena@users.noreply.github.com>
2024-07-09 17:39:58 +0200

    fix search

--- connessione_mongo_py.py
+++ connessione_mongo_py.py
@@ -17,14 +17,25 @@ db = client['ticket_one']
 collection = db['concerti']
 
 def ricerca_per_artista(artista):
-    concerti = collection.find({'artista.nome': artista})
+    concerti = collection.find({'artista.nome': artista}, {'_id':0, 'artista.tipo':1, 'artista.nome':1, 'nome_concerto': 1, 'artista.descrizione':1, 'luogo.nome':1, 'luogo.data':1, 'data':1})
     return list(concerti)
 
 
 def ricerca_per_concerto(concerto):
-    concerti = collection.find({'concerto.nome': concerto})
+    concerti = collection.find({'nome_concerto': concerto}, {'_id':0, 'artista.tipo':1 , 'artista.nome':1, 'nome_concerto': 1, 'artista.descrizione':1, 'luogo.nome':1, 'luogo.data':1, 'data':1})
     return list(concerti)
 
+def mostra_concerto(concerto):
+    print(f"""
+Nome: {concerto['nome_concerto']};
+Artista: {concerto['artista']['nome']};
+Descrizione: {concerto['artista']['descrizione']};
+Luogo: {concerto['luogo']['nome']};
+Data: {concerto['data']};""")
+
+    if concerto['artista']['tipo']=='band':
+        for membro in concerto['artista']['membri']:
+            print("- Nome:", membro['nome'], "| Strumento:", membro['strumento'])
 
 def main():
     while True:
@@ -39,14 +50,13 @@ def main():
             input_artista = input("Inserisci il nome dell'artista:  ")
             concerti_con_artista = ricerca_per_artista(input_artista)
             for concerto in concerti_con_artista:
-                print(concerti_con_artista)
+                mostra_concerto(concerto)
 
         elif scelta == '2':
             input_concerto = input("Inserisci il nome del concerto: ")
             concerti_con_nome = ricerca_per_concerto(input_concerto)
             for concerto in concerti_con_nome:
-                print(concerto)
-
+                mostra_concerto(concerto)
 
         elif scelta == '3':
             break
